f0551e7bc39265f2fc8260950e0fc0aeb39f0416
mranonno <anonnodas97@gmail.com>
2024-02-23 01:38:23 +0600

    filter

---
+++ filter.js
@@ -0,0 +1,6 @@
+const height = [57, 74, 67, 85, 56, 68];
+const large = height.filter(n =>n > 60 );
+console.log(large);
+const friends = ['Tom', 'John', 'Michel', 'Oliver'];
+const length = friends.filter(name=>name.length<4);
+console.log(length);
